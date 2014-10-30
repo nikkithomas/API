8ac7a3e6607e3cbfab1f6bc33f00848504a69f3a
nikkithomas <nikkithomas@Nikkis-MacBook-Pro.local>
2014-10-30 10:31:57 -0400

    2nd

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 $(document).ready(function(){
 	$('#term').focus(function(){
-		var full=$("#images").has("img").lenth?true:false;
+		var full=$("#ratings").has("").lenth?true:false;
 		if(full==false){
-			$('#images').empty();
+			$('#ratings').empty();
 		}
 	});
 
-	var getImages=fuction(){
-		var image=$('#term').val();
+	var getRatings=function(){
+		var rate=$('#term').val();
 
-		if(film==''){
-			$('#images').html("<h2 class='loading'>Please enter an actor or an actress.</h2");
+		if(rate==''){
+			$('#ratings').html("<h2 class='loading'>Please enter a movie title.</h2");
 
 		}else{
-			$('#images').html("<h2 class='loading'>Your image is coming!</h2>");
-			$.getJSON("")
+			$('#ratings').html("<h2 class='loading'>Your ratings are coming!</h2>");
+			$.getJSON("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=nhrg4r4r9wnp6ce46efr2cg&q="+rate+"3&page_limit=1?apikey=4nhrg4r4r9wnp6ce46efr2cg"/+rate+"&callback=?",function(json){
+				if(json.length>0){
+					$('#ratings').html('<h2 class="loading">We found ratings.</h2><span id="theRatings" src=' + json[0].ratings+'</span>');
+				} else {
+					$('#ratings').html('<h2 class="loading">We could not find anything</h2>');
+				}
+			});
 		}
+		return false;
 	}
 
+$('#search').click(getRatings);
+$('#term').keyup(function(event){
+	if(event.keyCode==13){
+		getRatings();
+	}
+});
 
 
-
-})
+});
